ccf51ce16dcf66aef9b15a21385338438c1eb286
leticialbuk <leticia.albuk@gmail.com>
2023-08-16 20:48:50 -0300

    Criando classes e organizando aplicação

---
+++ Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CRUD_Veiculos.Models
+{
+    public class Veiculo
+    {  
+        public Veiculo( string marca, string modelo, int ano, int preco)
+        {
+            //Id = id;
+            Marca = marca;
+            Modelo = modelo;
+            Ano = ano;
+            Preco = preco;
+        }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        //public string Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public int Preco { get; set; }
+
+    }
+}
